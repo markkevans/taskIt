7ff5f71c0daaf1aac1489bc4bd555332bc65b7f0
Mark Evans <markkevans@gmail.com>
2014-12-09 20:45:17 +0000

    Initial Commit

---
+++ Taskit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Taskit
+//
+//  Created by Mark Evans on 09/12/2014.
+//  Copyright (c) 2014 Mark Evans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
